b65df562ffab1888a1737e26662363da8b527323
John Cokos <highlander.44@gmail.com>
2019-08-17 08:28:38 -0700

    socket demo added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Create an instance of a socket server
-const io = require('socket.io')(3000);
+const port = process.env.PORT || 3000;
+const io = require('socket.io')(port);
 
 // allow connections
 io.on('connection', (socket) => {
